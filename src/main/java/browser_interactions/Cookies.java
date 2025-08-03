8ed9db435639c13088794d2637474079e34c80a4
sonali-shetty <46496240+sonali-shetty@users.noreply.github.com>
2025-08-03 08:02:59 +0000

    cookies

--- src/main/java/browser_interactions/Cookies.java
+++ src/main/java/browser_interactions/Cookies.java
@@ -8,6 +8,15 @@ public class Cookies extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+    Cookie cookie = new Cookie("theme","light");
+    driver.manage().addCookie(cookie);
+    //to access the cookies.
+   Cookie cookieTheme = driver.manage().getCookieNamed("theme");
+   String domain = cookieTheme.getDomain();
+   System.out.println(domain);
+
+   driver.manage().deleteCookie(cookieTheme);
+    System.out.println(driver.manage().getCookies().size());
 
 
 

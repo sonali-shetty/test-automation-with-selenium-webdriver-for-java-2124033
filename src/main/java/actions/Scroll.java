90c7932e962394867255b2c0c54f459b9041d877
sonali-shetty <46496240+sonali-shetty@users.noreply.github.com>
2025-08-03 10:02:42 +0000

    scroll height

--- src/main/java/actions/Scroll.java
+++ src/main/java/actions/Scroll.java
@@ -12,11 +12,15 @@ public class Scroll extends Main {
     // Scroll to element
     driver.get("https://www.selenium.dev/selenium/web/scroll.html");
 
+    var line9 = driver.findElement(By.id("line9"));
+    new Actions(driver).scrollToElement(line9).perform();
+
     
 
     //Scroll by amount
     driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/page_with_tall_frame.html");
-
+var height = driver.findElement(By.tagName("body")).getSize().getHeight();
+new Actions(driver).scrollByAmount(0, height).perform();
 
 
     driver.quit();

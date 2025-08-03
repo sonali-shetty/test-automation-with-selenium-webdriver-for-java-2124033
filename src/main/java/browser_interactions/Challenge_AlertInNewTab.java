cade53be986fbaffedca0ee94e9fd2c21516871f
sonali-shetty <46496240+sonali-shetty@users.noreply.github.com>
2025-08-03 08:35:14 +0000

    challenge alerts

--- src/main/java/browser_interactions/Challenge_AlertInNewTab.java
+++ src/main/java/browser_interactions/Challenge_AlertInNewTab.java
@@ -12,7 +12,21 @@ public class Challenge_AlertInNewTab extends Main {
   
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/alerts.html");
+    driver.findElement(By.id("open-window-with-onload-alert")).click();
 
+    var tabs = driver.getWindowHandles();
+    var currentTab = driver.getWindowHandle();
+
+    for(String tab : tabs){
+      driver.switchTo().window(tab);
+      System.out.println("switched to:"+ tab);
+      if(!currentTab.equals(driver.getWindowHandle())){
+        break;
+      }
+    }
+    Alert alert = driver.switchTo().alert();
+    System.out.println(alert.getText());
+    alert.accept();
 
 
     driver.quit();

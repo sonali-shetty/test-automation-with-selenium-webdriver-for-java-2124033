2774d6aab91e84e42fb630e47e9e17d7bc76fbfc
sonali-shetty <46496240+sonali-shetty@users.noreply.github.com>
2025-08-03 09:35:20 +0000

    double click actions api

--- src/main/java/actions/DoubleClick.java
+++ src/main/java/actions/DoubleClick.java
@@ -9,7 +9,9 @@ public class DoubleClick extends Main {
 
   public static void main(String[] args) {
     driver.get("https://selenium.dev/selenium/web/mouse_interaction.html");
-    
+    var input = driver.findElement(By.id("clickable"));
+    new Actions(driver).doubleClick(input).perform();
+    System.out.println(driver.findElement(By.id("click-status")).getText());
 
 
     driver.quit();

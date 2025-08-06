b205d13ded182511c9872c09ee148d7bd686d5fe
sonali-shetty <46496240+sonali-shetty@users.noreply.github.com>
2025-08-06 03:52:45 +0000

    login and base test

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -7,6 +7,20 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class BaseTests {
+  protected static WebDriver driver;
 
+  @BeforeAll
+  public static void setUp(){
+    chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    driver = new ChromeDriver(options);
+  }
+
+
+  @AfterAll
+  public static void tearDown(){
+    driver.quit();
+  }
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,7 +5,30 @@ import page_objects.LoginPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LoginTests {
+public class LoginTests extends BaseTests{
+
+  private LoginPage loginPage;
+
+  @BeforeEach
+  public void loadPage(){
+    driver.get("https://www.selenium.dev/selenium/web/login.html");
+    loginPage = new LoginPage(driver);
+
+  }
+
+  @Test
+  public void testSuccessfulLogin(){
+    String message = loginPage.login("username","password");
+    assertEquals("You have successfully logged in.", message);
+  }
+
+  @Test 
+  public void loginWithNoUsername(){
+    String message = loginPage.login("", "password");
+    assertEquals("Please enter valid credentials", message);
+  }
+
+
 
 
 }
